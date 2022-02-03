571ed32ea36afd5378d18a5a8b6fa95033e7e185
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-02-03 11:56:03 +0100

    Initialized the different pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './reset.css';
+import './style.css';
+import Header from 'components/Header';
+import Navbar from 'components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from 'components/Home';
+import SearchPage from 'components/SearchPage';
+import Details from 'components/Details';
 
 function App() {
-  return <h1>Hello there!</h1>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/searchpage" element={<SearchPage />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
